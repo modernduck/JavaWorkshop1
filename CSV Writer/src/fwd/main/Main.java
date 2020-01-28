dad47aa4999d6f64646ec03ed598b2125211f193
sompopkulapalanont <sompopkulapalanont@sompops-mbp>
2020-01-28 15:50:38 +0700

    update path of directory

--- CSV Writer/src/fwd/main/Main.java
+++ CSV Writer/src/fwd/main/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		String path = "/Users/sompopkulapalanont/Projects/java-course/test.csv";
+		String path = "C:\\adsasdasdsad";
 		
 		
 		
